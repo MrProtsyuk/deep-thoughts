ba50dd91b997c91158e8d984d7a36a76162167ea
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-11 15:19:01 -0700

    implimented middleware into auth and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,19 +4,36 @@ const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('__v -password')
+            .populate('thoughts')
+            .populate('friends');
+
+            return userData;
+
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
         },
+
         thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
         },
+
         users: async () => {
             return User.find()
               .select('-__v -password')
               .populate('friends')
               .populate('thoughts');
           },
+
           // get a user by username
           user: async (parent, { username }) => {
             return User.findOne({ username })
@@ -24,7 +41,9 @@ const resolvers = {
               .populate('friends')
               .populate('thoughts');
           },
+
     },
+
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
@@ -32,6 +51,7 @@ const resolvers = {
 
             return { token, user };
         },
+
         login: async (parent, {email, password }) => {
             const user = await User.findOne({ email });
 
