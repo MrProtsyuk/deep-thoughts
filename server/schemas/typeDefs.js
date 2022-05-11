17cd307c44606b594c16cdfcf97fbbb9b402d510
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-11 14:43:57 -0700

    added login mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,7 +13,7 @@ const typeDefs = gql`
         thoughts: [Thought]
         friends: [User]
     }
-    
+
     type Thought {
         _id: ID
         thoughtText: String
@@ -35,6 +35,11 @@ const typeDefs = gql`
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
     }
     `;
 
