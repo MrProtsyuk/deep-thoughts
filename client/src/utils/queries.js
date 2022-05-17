789f19215d4da3111f4ef747d7687bac140aa7e2
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-16 22:11:48 -0700

    refreshed files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -34,26 +34,67 @@ export const QUERY_THOUGHT = gql`
       }
     }
   }
-  `;
+`;
 
-  export const QUERY_USER = gql`
-    query user($username: String!) {
-      user(username: $username) {
-        _id 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
         username
-        email
-        friendCount
-        friends {
-          _id
-          username
-        }
-        thoughts {
-          _id
-          thoughtText
-          createdAt
-          reactionCount
-        }
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
-  `
-  ;
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
